aa0943a9de2bbb8e11bca2c07ca3e55d02aeefef
Kushal Das <kushaldas@gmail.com>
2014-08-05 11:04:36 +0530

    updated testdata creation.

--- testdata.py
+++ testdata.py
@@ -3,25 +3,17 @@
 __requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
 import pkg_resources
 
-from bugspad2 import APP
-from bugspad2.default_config import DB_URL
-from bugspad2.lib import model
-import bugspad2.lib as mmlib
+from ukhra import APP
+from ukhra.default_config import DB_URL
+from ukhra.lib import model
+import ukhra.lib as mmlib
 from redis import Redis
 import json
 import hashlib
+from datetime import datetime
 
 r = Redis()
 SESSION = mmlib.create_session(DB_URL)
-
-pr = model.Product(name='Fedora', description='OS')
-SESSION.add(pr)
-SESSION.commit()
-ver = model.Version(name='20', product_id=1, active=True)
-SESSION.add(ver)
-SESSION.commit()
-data = [(1, '20'),]
-r.set('product:1', json.dumps(data))
 
 #Now add a user too.
 password = 'asdf%s' % APP.config.get('PASSWORD_SEED', None)
@@ -29,12 +21,38 @@ password = hashlib.sha512(password).hexdigest()
 user = model.User(user_name='kdas',email_address='kushaldas@gmail.com',display_name='Kushal Das', password=password)
 SESSION.add(user)
 SESSION.commit()
-f = open('comps.json')
-comps = json.load(f)
-f.close()
-print 'Adding components.'
-for comp in comps:
-    c = model.Component(name=comp['name'], description=comp['description'], product_id=1)
-    SESSION.add(c)
-    SESSION.commit()
+
+group = model.Group(group_name='admin',display_name='Admin')
+SESSION.add(group)
+SESSION.commit()
+
+ugbase = model.UserGroup(user_id=user.id, group_id=group.id)
+SESSION.add(ugbase)
+SESSION.commit()
+
+
+
+helptext = u'''Welcome to the new wiki system of dgplug.
+
+### How to create a new page?
+
+
+Just type in the URL, if the pages does not exist then it will ask you to start a new one.
+Remember to add some summary to each change.
+
+
+
+### Tags are not showing!!!
+
+
+Yes, we are yet to add that feature. '''
+
+page = model.Page(data=helptext, created=datetime.now(), updated=datetime.now(), path='help', writer=1, version=0)
+
+SESSION.add(page)
+SESSION.commit()
+
+
+
+r.flushall()
 mmlib.load_all(SESSION)
